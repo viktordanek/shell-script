780e955280f643ce89dfe05c3a40bd05fac34813
Viktor Danek <viktordanek10@gmail.com>
2025-03-18 15:24:06 -0400

    YES

--- scripts/foobar.sh
+++ scripts/foobar.sh
@@ -2,6 +2,5 @@ SINGLETON=$( ${CAT} /singleton/file ) &&
   STANDARD_INPUT=$( ${CAT} ) &&
   ${CHMOD} 0777 /singleton/file &&
   ${ECHO} singleton ${SINGLETON} ${STANDARD_INPUT} > /singleton/file &&
-  stat /singleton/file > /singleton/stat &&
   ${ECHO} standard-output ${SINGLETON} ${STANDARD_INPUT} &&
   ${ECHO} standard-error ${SINGLETON} ${STANDARD_INPUT} >&2
