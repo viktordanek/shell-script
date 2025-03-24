bbe1b77bbee0ba979ab585b471b32cba9e83fc11
Viktor Danek <viktordanek10@gmail.com>
2025-03-24 12:32:32 +0000

    success

--- vacuum.sh
+++ vacuum.sh
@@ -9,7 +9,7 @@ INPUT=${1} &&
         HASH=$( ${ECHO} ${KEY} ${UUID} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
         INDEX=$( ${FIND} ${OUTPUT}/${HASH} -mindepth 0 -maxdepth 0 -type f -name "${HASH}.*.key" | ${WC} --lines ) &&
         ${CAT} ${FILE} > ${OUTPUT}/${HASH}.${INDEX}.cat &&
-        ${STAT} ${FILE} > ${OUTPUT}/${HASH}.${INDEX}.stat &&
+        ${STAT} --format "%a" ${FILE} > ${OUTPUT}/${HASH}.${INDEX}.stat &&
         ${ECHO} /${NAME}/${KEY} > ${OUTPUT}/${HASH}.${INDEX}.key
         ${CHMOD} 0777 ${OUTPUT}/${HASH}.${INDEX}.cat ${OUTPUT}/${HASH}.${INDEX}.stat ${OUTPUT}/${HASH}.${INDEX}.key
     done
