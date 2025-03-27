be66ff969e82e585e7a9b3a020df33d7a86daeee
Viktor Danek <viktordanek10@gmail.com>
2025-03-27 17:13:01 +0000

    I redid tests.  I got rid of environment-variable.  I added champion.  I made environment better.  i made tests more robust but not good enough.

---
+++ vacuum.sh
@@ -0,0 +1,14 @@
+INPUT=${1} &&
+  OUTPUT=${2} &&
+  NAME=${3} &&
+  ${MKDIR} ${OUTPUT} &&
+  ${FIND} ${INPUT} | while read FILE
+  do
+    KEY=${FILE#${INPUT}} &&
+      HASH=$( ${ECHO} ${KEY} ${UUID} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
+      INDEX=$( ${FIND} ${OUTPUT}/${HASH} -mindepth 0 -maxdepth 0 -type f -name "${HASH}.*.key" | ${WC} --lines ) &&
+      ${CAT} ${FILE} > ${OUTPUT}/${HASH}.${INDEX}.cat &&
+      ${STAT} --format "%a" ${FILE} > ${OUTPUT}/${HASH}.${INDEX}.stat &&
+      ${ECHO} ${NAME}/${KEY} > ${OUTPUT}/${HASH}.${INDEX}.key &&
+      ${CHMOD} 0777 ${OUTPUT}/${HASH}.${INDEX}.cat ${OUTPUT}/${HASH}.${INDEX}.stat ${OUTPUT}/${HASH}.${INDEX}.key
+  done
