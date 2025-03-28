da8321a74f81271ea33c0e24be95aa7281b6d8e9
Viktor Danek <viktordanek10@gmail.com>
2025-03-28 21:21:00 +0000

    WTF

--- foobar.sh
+++ foobar.sh
@@ -1,6 +1,6 @@
 SINGLETON=$( ${CAT} /singleton ) &&
   STANDARD_INPUT=$( ${CAT} ) &&
-  ${ECHO} singleton ${SINGLETON} ${STANDARD_INPUT} ${@} ${FOOBAR}  > /singleton &&
+  ${ECHO} singleton ${SINGLETON} ${STANDARD_INPUT} ${@} ${FOOBAR} > /singleton &&
   ${ECHO} standard-output ${SINGLETON} ${STANDARD_INPUT} ${@} ${FOOBAR} &&
   ${ECHO} standard-error ${SINGLETON} ${STANDARD_INPUT} ${@} ${FOOBAR} >&2 &&
   exit $(( 0x$( ${ECHO} ${SINGLETON} ${STANDARD_INPUT} ${@} ${FOOBAR} | ${SHA512SUM} | ${CUT} --bytes -128 ) % 256 ))
