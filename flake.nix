91eadbf69677a98a78461325d020cb424864b1e0
Viktor Danek <viktordanek10@gmail.com>
2025-03-30 16:31:30 +0000

    progress

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
                                                     else builtins.throw "profile is not lambda but ${ builtins.typeOf profile }." ;
                                                 script =
                                                     if builtins.typeOf script == "string" then
-                                                        if builtins.pathExists script then pkgs.writeShellScript "script" script
+                                                        if builtins.pathExists script then pkgs.writeShellScript "script" ( builtins.readFile script )
                                                         else builtins.throw "there is no path for ${ script }."
                                                     else builtins.throw "script is not string but ${ builtins.typeOf script }." ;
                                                 tests =
