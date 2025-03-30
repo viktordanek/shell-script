54c0264c1362a1852f48d8e1c31cf4848b35b4f4
Viktor Danek <viktordanek10@gmail.com>
2025-03-30 16:35:27 +0000

    cleanup

--- flake.nix
+++ flake.nix
@@ -106,8 +106,8 @@
                                                     {
                                                         extraBwrapArgs = builtins.attrValues ( builtins.mapAttrs ( name : { host-path , is-read-only , ... } : "${ if is-read-only then "--ro-bind" else "--bind" } ${ host-path } ${ name }" ) mounts ) ;
                                                         name = name ;
-                                                        profile = builtins.trace "PROFILE< ${ profile } >" profile ;
-                                                        runScript = builtins.trace ( builtins.toString primary.script ) ( builtins.toString primary.script ) ;
+                                                        profile = profile ;
+                                                        runScript = builtins.toString primary.script ;
                                                     } ;
                                         in
                                             {
