5e78c40882a203015db63453444c932f20927bd4
Viktor Danek <viktordanek10@gmail.com>
2025-03-31 15:22:42 +0000

    remove champion and environment

--- flake.nix
+++ flake.nix
@@ -15,8 +15,6 @@
                             _environment-variable = builtins.getAttr system environment-variable.lib ;
                             lib =
                                 {
-                                    champion ? null ,
-                                    environment ? x : [ ] ,
                                     extensions ? [ ] ,
                                     mounts ? { } ,
                                     name ,
@@ -27,33 +25,6 @@
                                     let
                                         primary =
                                             {
-                                                champion =
-                                                    if builtins.typeOf champion == "null" then champion
-                                                    else if builtins.typeOf champion == "set" then
-                                                        let
-                                                            identity =
-                                                                { environment ? x : [ ] , script } :
-                                                                    {
-                                                                        environment =
-                                                                            if builtins.typeOf environment == "lambda" then
-                                                                                if builtins.typeOf ( environment primary.extensions ) == "list" then
-                                                                                    builtins.map ( e : if builtins.typeOf e == "string" then e else builtins.throw "environment is not string but ${ builtins.typeOf e }." ) environment primary.extensions
-                                                                                else builtins.throw "champion environments is not list but ${ builtins.typeOf ( environment primary.extension ) }."
-                                                                            else builtins.throw "champion environments is not lambda but ${ builtins.typeOf environment }." ;
-                                                                        script =
-                                                                            if builtins.typeOf script == "string" then
-                                                                                if builtins.pathExists script then script
-                                                                                else builtins.throw "there is no path for champion ${ script }."
-                                                                            else builtins.throw "champion script is not string but ${ builtins.typeOf script }." ;
-                                                                    } ;
-                                                            in identity champion
-                                                    else builtins.throw "champion is not null, set but ${ builtins.typeOf champion }." ;
-                                                environment =
-                                                    if builtins.typeOf environment == "lambda" then
-                                                        if builtins.typeOf ( environment primary.extensions ) == "list" then
-                                                            builtins.map ( e : if builtins.typeOf e == "string" then e else builtins.throw "environment is not string but ${ builtins.typeOf e }." ) ( environment primary.extensions )
-                                                        else builtins.throw "environments is not list but ${ builtins.typeOf ( environment primary.extension ) }."
-                                                    else builtins.throw "environments is not lambda but ${ builtins.typeOf environment }." ;
                                                 extensions =
                                                     if builtins.typeOf extensions == "set" then
                                                         builtins.mapAttrs ( name : value : if builtins.typeOf value == "lambda" then value else builtins.throw "extension is not lambda but ${ builtins.typeOf value }." ) extensions
