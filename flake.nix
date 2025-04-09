ee6b26139f180dc912b66566e942c28d14ef2773
Viktor Danek <viktordanek10@gmail.com>
2025-04-09 11:19:31 +0000

    restart

--- flake.nix
+++ flake.nix
@@ -451,6 +451,28 @@
                                                                             script = self + "/foobar.sh" ;
                                                                             tests =
                                                                                 {
+                                                                                    delayed =
+                                                                                        ignore :
+                                                                                            {
+                                                                                                mounts =
+                                                                                                    {
+                                                                                                        "/singleton" =
+                                                                                                            {
+                                                                                                                expected = self + "/expected/foobar/file/mounts/singleton" ;
+                                                                                                                initial =
+                                                                                                                    [
+                                                                                                                        "echo 0d157cd5708ec01d0b865b8fbef69d7b28713423ec011a86a5278cf566bcbd8e79a2daa996d7b1b8224088711b75fda91bdc1d41d0e53dd7118cfbdec8296044 > /mount/target"
+                                                                                                                    ] ;
+                                                                                                            } ;
+                                                                                                    } ;
+                                                                                                standard-error = self + "/expected/foobar/file/standard-error" ;
+                                                                                                standard-output = self + "/expected/foobar/file/standard-output" ;
+                                                                                                status = 168 ;
+                                                                                                test =
+                                                                                                    [
+                                                                                                        "candidate 2a6273b589f1a8b3ee9e5ad7fc51941863a0b5a8ed1eebe444937292110823579f4b9eb6c72d096012d4cf393335d7e8780ec7ec5d02579aabe050f22ebe2201"
+                                                                                                    ] ;
+                                                                                            } ;
                                                                                     directory =
                                                                                         ignore :
                                                                                             {
@@ -512,7 +534,8 @@
                                                                             else
                                                                                 ${ pkgs.coreutils }/bin/echo "There was an unpredicted failure in ${ foobar.tests }" >&2 &&
                                                                                     exit 62
-                                                                            fi
+                                                                            fi &&
+                                                                            exit 61
                                                                     '' ;
                                                         name = "foobar" ;
                                                         src = ./. ;
