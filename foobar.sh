c0fb34a2921fd9e6010a1d5fe72d1bce4e41744a
Viktor Danek <viktordanek10@gmail.com>
2025-04-11 20:56:22 -0400

    almost

--- foobar.sh
+++ foobar.sh
@@ -8,6 +8,10 @@ if [ -f /singleton ]
   if [ -f /singleton ]
   then
     ${ECHO} singleton ${SINGLETON} ${STANDARD_INPUT} ${@} > /singleton
+  elif [ -d /singleton ]
+  then
+    ${ECHO} file ${SINGLETON} > /singleton/file &&
+     ${CHMOD} 0777 /singleton/file
   fi &&
   ${ECHO} standard-output ${SINGLETON} ${STANDARD_INPUT} ${@} &&
   ${ECHO} standard-error ${SINGLETON} ${STANDARD_INPUT} ${@} >&2 &&
