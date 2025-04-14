50f0c6f3a0e3559f3d85a93df4ee6fbc12a61aa5
Viktor Danek <viktordanek10@gmail.com>
2025-04-14 08:08:28 -0400

    finding error

--- observe.sh
+++ observe.sh
@@ -1,44 +1,45 @@
 ${MKDIR} /work/mounts &&
-  ${FIND} ${OUT} -type f -name "initial.*.shelled.sh" | while read FILE
+  ${FIND} ${OUT}/bin -type l -name "initial.*.shelled.sh" | ${SORT} | while read FILE
   do
     NAME=$( ${BASENAME} "$FILE" ) &&
       HASH_1=${NAME#initial.} &&
-      HASH=${HASH_1%.wrapped.sh} &&
+      HASH=${HASH_1%.shelled.sh} &&
       ${MKDIR} /work/mounts/${HASH} &&
       ${MKDIR} /work/mounts/${HASH}/mount &&
-      if ${FILE} > /work/mounts/${HASH}/standard-output 2> /work/mounts/${HASH}/standard-error
+      ${LN} --symbolic ${OUT}/bin/initial.${HASH}.sh /work/mounts/${HASH}.sh &&
+      if ${FILE} > /work/mounts/${HASH}.standard-output 2> /work/mounts/${HASH}.standard-error
       then
         ${ECHO} ${?} > /work/mounts/${HASH}/status
       else
         ${ECHO} ${?} > /work/mounts/${HASH}/status
       fi &&
-      if [ ! -e /work/mounts/${HASH}/standard-output ]
+      if [ ! -e /work/mounts/${HASH}.standard-output ]
       then
         ${ECHO} missing standard-output for ${HASH} >> /work/ERROR
-      elif [ ! -z "$( ${CAT} /work/mounts/standard-output ) }" ]
+      elif [ ! -z "$( ${CAT} /work/mounts/${HASH}.standard-output ) }" ]
       then
         ${ECHO} non-empty standard-output for ${HASH} >> /work/ERROR &&
-          ${CAT} /work/mounts/standard-output >> /work/ERROR
-      elif [ ! -e /work/mounts/${HASH}/standard-error ]
+          ${CAT} /work/mounts/${HASH}.standard-output >> /work/ERROR
+      elif [ ! -e /work/mounts/${HASH}.standard-error ]
       then
         ${ECHO} missing standard-error for ${HASH} >> /work/ERROR
-      elif [ ! -z "$( ${CAT} /work/mounts/standard-error ) }" ]
+      elif [ ! -z "$( ${CAT} /work/mounts/${HASH}.standard-error ) }" ]
       then
         ${ECHO} non-empty standard-error for ${HASH} >> /work/ERROR &&
-          ${CAT} /work/mounts/standard-error >> /work/ERROR
+          ${CAT} /work/mounts/${HASH}.standard-error >> /work/ERROR
       elif [ ! -e /work/mounts/${HASH}/status ]
       then
         ${ECHO} missing status for ${HASH} >> /work/ERROR
-      elif [ "$( ${CAT} /work/mounts/status != 0 ) }" ]
+      elif [ "$( ${CAT} /work/mounts/${HASH}/status != 0 ) }" ]
       then
         ${ECHO} non-zero status for ${HASH} >> /work/ERROR &&
-          ${CAT} /work/mounts/status >> /work/ERROR
+          ${CAT} /work/mounts/${HASH}/status >> /work/ERROR
       elif [ ! -e /work/mounts/${HASH}/mount/target ]
       then
         ${ECHO} initial did not create target >> /work/ERROR
-      elif [ $( ${FIND} /work/mounts -mindepth 1 -maxdepth 1 ! -name standard-output ! -name standard-error ! -name status ! -name mount | ${WC} --lines ) != 0 ]
+      elif [ $( ${FIND} /work/mounts/${HASH}/mount -mindepth 1 -maxdepth 1 ! -name target | ${SORT} | ${WC} --lines ) != 0 ]
       then
         ${ECHO} initial over created >> /work/ERROR &&
-          ${FIND} /work/mounts -mindepth 1 -maxdepth 1 ! -name standard-output ! -name standard-error ! -name status ! -name mount >> /work/ERROR
+          ${FIND} /work/mounts -mindepth 1 -maxdepth 1 ! -name standard-output ! -name standard-error ! -name status ! -name mount | ${SORT} >> /work/ERROR
       fi
   done
