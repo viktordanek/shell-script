f59f9f506fbcfcaeee2ab9ae2362f6525fa66856
Viktor Danek <viktordanek10@gmail.com>
2025-04-16 15:15:24 -0400

    baby steps

--- flake.nix
+++ flake.nix
@@ -260,6 +260,7 @@
                                                                                                                                         )
                                                                                                                                         [
                                                                                                                                             "${ _environment-variable "LN" } --symbolic ${ secondary.test } ${ _environment-variable "OUT" }/bin/test.sh"
+                                                                                                                                            "makeWrapper ${ _environment-variable "OUT" }/bin/test.sh ${ _environment-variable "OUT" }/bin/test.wrapped.sh"
                                                                                                                                         ]
                                                                                                                                         [
                                                                                                                                             (
