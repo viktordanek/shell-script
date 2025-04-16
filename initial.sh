7a9dca301d0d830be201ed84181dbb90df25d16d
Viktor Danek <viktordanek10@gmail.com>
2025-04-16 09:23:41 -0400

    baby steps

--- initial.sh
+++ initial.sh
@@ -4,10 +4,16 @@ then
 else
   ${ECHO} ${?} > /record/status
 fi &&
-  if [ ! -e /record/standard-output ]
+  if [ ! -f /record/standard-output ]
   then
     ${ECHO} missing standard-output >> /record/ERROR
   elif [ ! -z "$( ${CAT} /record/standard-output )" ]
   then
     ${ECHO} non-empty standard-output >> /record/ERROR
+  elif [ ! -f /record/standard-error ]
+  then
+    ${ECHO} missing standard-error >> /record/ERROR
+  elif [ ! -z "$( ${CAT} /record/standard-error )" ]
+  then
+    ${ECHO} non-empty standard-error >> /record/ERROR
   fi
