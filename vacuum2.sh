0f7bcb0d8e64ea7e860e00db14cd6d0f1fb21f4e
Viktor Danek <viktordanek10@gmail.com>
2025-04-20 09:30:32 -0400

    baby progress

--- vacuum2.sh
+++ vacuum2.sh
@@ -2,18 +2,23 @@ ${FIND} /input | while read FILE
   do
     KEY=${FILE#/input} &&
       HASH=$( ${ECHO} ${KEY} ${UUID} | ${SHA512SUM} | ${CUT} --bytes -128 ) &&
-      INDEX=$( ${FIND} /output -mindepth 2 -maxdepth 2 -type d -name "${HASH}" | ${WC} --lines ) &&
-      if [ ${INDEX} == 0 ]
+      if [ -d /output/0 ]
       then
-        ${MKDIR} /output/${HASH}
+        INDEX=$( ${FIND} /output -mindepth 2 -maxdepth 2 -type d -name "${HASH}" | ${WC} --lines )
+      else
+        INDEX=0
       fi &&
-      ${MKDIR} /output/${HASH}/${INDEX} &&
-      ${ECHO} ${KEY} > /output/${HASH}/${INDEX}/key &&
-      ${STAT} --format "%a" ${FILE} > /output/${HASH}/${INDEX}/stat &&
-      ${CHMOD} 0777 /output/${HASH}/${INDEX}/key /output/${HASH}/${INDEX}/stat &&
+      if [ ! -d /output/${INDEX} ]
+      then
+        ${MKDIR} /output/${INDEX}
+      fi &&
+      ${MKDIR} /output/${INDEX}/${HASH} &&
+      ${ECHO} ${KEY} > /output/${INDEX}/${HASH}/key &&
+      ${STAT} --format "%a" ${FILE} > /output/${INDEX}/${HASH}/stat &&
+      ${CHMOD} 0777 /output/${INDEX}/${HASH}/key /output/${INDEX}/${HASH}/stat &&
       if [ -f ${FILE} ]
       then
-        ${CAT} ${FILE} > /output/${HASH}/${INDEX}/cat
-          ${CHMOD} 0777 /output/${HASH}/${INDEX}/cat
+        ${CAT} ${FILE} > /output/${INDEX}/${HASH}/cat
+          ${CHMOD} 0777 /output/${INDEX}/${HASH}/cat
       fi
   done
