09d3b19686938c55c83f361e65e0ff1b72ed78a7
Viktor Danek <viktordanek10@gmail.com>
2025-04-23 15:24:42 -0400

    I think I see the problem. candidate is not in the path

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
+${ECHO} AAAA ${TEST}  >> /work/DEBUG &&
 if ${TEST} > /work/final/standard-output 2> /work/final/standard-error
 then
+${ECHO} AAAB >> /work/DEBUG &&
   ${ECHO} ${?} > /work/final/status
 else
+${ECHO} AAAC >> /work/DEBUG &&
   ${ECHO} ${?} > /work/final/status
 fi
